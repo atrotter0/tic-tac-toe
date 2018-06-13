4d0bab4691b86d1c1fa154d9ffa73321bacafae8
Abel Trotter and Thad Donaghue <thad@>
2018-06-13 11:47:25 -0700

    Add win conditions and loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 function Player(name) {
   this.name = name;
   this.difficulty = "";
-  this.playerMark = "";
+  this.mark = "";
   this.turn = false;
+  this.selections = [];
 }
 
 function Board(coordinates) {
@@ -15,7 +16,7 @@ Board.prototype.determineWhoGoesFirst = function(player1, player2) {
   var coinFlip = Math.floor((Math.random() * 2) + 1);
   console.log("Coin Flip: " + coinFlip);
   if (coinFlip === 1) {
-    player1.turn = true
+    player1.turn = true;
     this.whoGoesFirst = player1.name;
   } else {
     player2.turn = true;
@@ -29,13 +30,66 @@ Board.prototype.announceTurn = function() {
 }
 
 Board.prototype.placeMark = function(player, coordinate) {
-  this.coordinates.coordinate = player.mark;
+  this.coordinates[coordinate] = player.mark;
+  player.selections.push(coordinate);
   this.lastMarkPlaced = coordinate;
   this.checkForWin();
 }
 
-Board.prototype.checkForWin = function() {
+Board.prototype.checkForWin = function(player1, player2) {
+  // horizontal three of a kind A1 A2 A3 ... C1 C2 C3
+  // vertical three of a kind A1 B1 C1 ... A3 B3 C3
+  // diagonal one of a kind A1 B2 C3 / C1 B2 A3
+  if (this.threeOfAKind(player1)) {
+    console.log("win!");
+  }
+}
 
+Board.prototype.threeOfAKind = function(player) {
+  var result = false;
+  var counter = 0;
+  var horiz1 = ["a1", "a2", "a3"];
+  var horiz2 = ["b1", "b2", "b3"];
+  var horiz3 = ["c1", "c2", "c3"];
+  var vert1 = ["a1", "b1", "c1"];
+  var vert2 = ["a2", "b2", "c2"];
+  var vert3 = ["a3", "b3", "c3"];
+  var diag1 = ["a1", "b2", "c3"];
+  var diag2 = ["c1", "b2", "a3"];
+  var winConditions = [horiz1, horiz2, horiz3, vert1, vert2, vert3, diag1, diag2];
+
+  console.log("Made it here!");
+  for(var i = 0; i < winConditions.length; i++) {
+    console.log(winConditions[i]);
+    for(j = 0; j < winConditions[j].length; j++) {
+      console.log(winConditions[i][j]);
+      if (counter >= 3) {
+        console.log("Current j: " + j);
+        console.log("Counter: " + counter);
+        return true;
+      } else {
+        counter = 0;
+      }
+      for(k = 0; k < player.selections.length; k++) {
+        console.log(counter);
+        if (winConditions[i][j].includes(player.selections[k])) {
+          counter++;
+        }
+      }
+    }
+  }
+}
+
+Board.prototype.checkArrayValues = function(array, selections) {
+  var counter = 0;
+  for(var i = 0; i < array.length; i++) {
+    if (array.includes(selections[i])) counter++;
+  }
+  return this.checkCounter(counter);
+}
+
+Board.prototype.checkCounter = function(counter) {
+  if (counter === 3) return true;
 }
 
 function Coordinate() {
